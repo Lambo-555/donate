bdf26b269607d3fd5cebda92e637aa6a510b2c68
Moostanger <lambo-555@yandex.ru>
2020-09-14 12:32:54 +0300

    Build

---
+++ build/precache-manifest.971dfa90db53fb01365598b6046410ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a015c87a36bf184985a3efaf22d69de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e823c6e150872d505f84",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "e823c6e150872d505f84",
+    "url": "/static/js/2.8b933ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b933ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51245390bac1fe9a1357",
+    "url": "/static/js/main.ebfbf626.chunk.js"
+  },
+  {
+    "revision": "a1d9bd6bba9f44cdbb90",
+    "url": "/static/js/runtime-main.cdbff447.js"
+  }
+]);
